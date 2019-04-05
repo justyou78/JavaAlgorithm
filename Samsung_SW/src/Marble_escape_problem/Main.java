d2d202231299d7317678ac510930c169721287f1
Andonggue <justyou567@naver.com>
2019-04-05 21:34:08 +0900

    recently work

--- Samsung_SW/src/Marble_escape_problem/Main.java
+++ Samsung_SW/src/Marble_escape_problem/Main.java
@@ -1,7 +1,10 @@
 package Marble_escape_problem;
 
+import java.io.IOException;
+
 public class Main {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		Marble_escape m_e= new Marble_escape();
+		m_e.exe();
 	}
 }
